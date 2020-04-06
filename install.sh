ddee30a276172dab469150cf7222cf7e9b615863
U-LINKOVSKY-PC\George <krougz@live.ru>
2020-04-06 22:51:41 +0400

    Add email :\

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp bashrc ~/.bashrc
 cp vimrc/vimrc ~/.vimrc
 
+git config --global user.email "krougz@live.ru"
+
 clear && echo "Done" && exec bash
--- install_android.sh
+++ install_android.sh
@@ -6,4 +6,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp bashrc ~/.bashrc
 cp vimrc/vimrc_android ~/.vimrc
 
+git config --global user.email "krougz@live.ru"
+
 clear && echo "Done" && exec bash
